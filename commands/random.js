cd54508517596bff125983ea638b61ca3108972a
BillCurdMusic <45gzj9sm7r@privaterelay.appleid.com>
2025-12-11 01:05:10 -0600

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -1,1 +1,13 @@
+export const data = {
+  name: "random",
+  description: "Play a random track from the playlist."
+};
 
+export async function execute(interaction, playlist, playRandom, joinChannel) {
+  if (!playlist.length) {
+    return interaction.reply("Ain’t got no tracks yet, kid.");
+  }
+
+  joinChannel(interaction);
+  setTimeout(() => playRandom(interaction), 500);
+}
